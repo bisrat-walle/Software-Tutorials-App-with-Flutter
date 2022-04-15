e98ab21e663338edc11f21f56884947b0b484641
bisrat-walle <bisratwalle3@gmail.com>
2022-04-15 20:48:56 +0300

    adding startup files

---
+++ src/main/java/com/groupproject/softwaretutorials/SoftwaretutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.groupproject.softwaretutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwaretutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwaretutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupproject/softwaretutorials/SoftwaretutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupproject.softwaretutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwaretutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
