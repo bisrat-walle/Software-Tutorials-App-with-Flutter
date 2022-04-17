19453e3541ed34d25eeeb3e3e0a40c8321a065c7
bisrat-walle <bisratwalle3@gmail.com>
2022-04-17 09:36:45 +0300

    adding a material app home screen

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/screens.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
