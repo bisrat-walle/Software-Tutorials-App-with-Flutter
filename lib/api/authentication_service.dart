7148565c7c771dd6bf2571a30eb7c792dfc6e90f
bisrat-walle <bisratwalle3@gmail.com>
2022-04-17 23:06:51 +0300

    resolving api request issue

--- lib/api/authentication_service.dart
+++ lib/api/authentication_service.dart
@@ -2,24 +2,25 @@ import 'package:http/http.dart' as http;
 import '../models/login_response.dart';
 import 'dart:convert';
 
-String _baseUrl = "http://localhost:8080/api/v1";
-Future<LoginResponse> authenticateUser({String? username, String? password}) async {
-  final client = http.Client();
-  final response = await client.post(
-    Uri.parse(_baseUrl+"/login"),
-    headers: <String, String>{
-      'Content-Type': 'application/json',
-	  'Accept': 'application/json'
-    },
-    body: jsonEncode(<String, String?>{
-      'username': username,
-      'password': password
-    }),
-  );
-
-  if (response.statusCode == 200) {
-    return LoginResponse.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to authenticate!');
+String _baseUrl = "http://localhost:8080/api/v1/login";
+Future<LoginResponse?> authenticateUser({String? username, String? password}) async {
+  try{
+		final response = await http.post(
+		Uri.parse(_baseUrl),
+		headers: <String, String>{
+		  'Content-Type': 'application/json',
+		  'accept': '*/*'
+		},
+		body: jsonEncode(<String, String?>{
+		  'username': username,
+		  'password': password
+		}),
+	  );
+	
+	  if (response.statusCode == 200) {
+		return LoginResponse.fromJson(jsonDecode(response.body));
+	  }
+  } catch(e){
+	print(e);
   }
 }
