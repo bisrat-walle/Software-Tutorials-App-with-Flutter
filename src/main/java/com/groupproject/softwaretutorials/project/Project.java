b3f41127fc86939eee56c7ccf6071de8bdcbf07e
ysihak-B <ysihakbazezew@gmail.com>
2022-04-19 20:00:53 +0300

    adding repo for projects

--- src/main/java/com/groupproject/softwaretutorials/project/Project.java
+++ src/main/java/com/groupproject/softwaretutorials/project/Project.java
@@ -3,8 +3,6 @@ package com.groupproject.softwaretutorials.project;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.Data;
 import java.io.Serializable;
 
