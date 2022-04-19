01fb2dedf108820adfc638d9606694c3218a2d29
ysihak-B <ysihakbazezew@gmail.com>
2022-04-19 19:48:03 +0300

    adding project class

---
+++ src/main/java/com/groupproject/softwaretutorials/project/Project.java
@@ -0,0 +1,28 @@
+package com.groupproject.softwaretutorials.project;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Data;
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Data
+public class Project implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long projectId;
+
+    
+    @Column(length = 50)
+    private String title;
+
+    
+    @Column(length = 1000)
+    private String problemStatement;
+}
