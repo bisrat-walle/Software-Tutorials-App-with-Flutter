fcce0839827b621d7af683e19d5de5ad44a77c01
bisrat-walle <bisratwalle3@gmail.com>
2022-05-18 21:45:47 +0300

    fixing small drawback in tutorial representation model

--- src/main/java/com/groupproject/softwaretu/apiv1/tutorial/TutorialRepresentation.java
+++ src/main/java/com/groupproject/softwaretu/apiv1/tutorial/TutorialRepresentation.java
@@ -2,6 +2,7 @@ package com.groupproject.softwaretu.apiv1.tutorial;
 
 import com.groupproject.softwaretu.tutorial.Tutorial;
 import com.groupproject.softwaretu.security.User;
+import com.groupproject.softwaretu.project.Project;
 import lombok.Data;
 
 
@@ -11,11 +12,13 @@ public class TutorialRepresentation {
     public String title;
     public String content;
 	public User instructor;
+	public Project project;
     
     public TutorialRepresentation(Tutorial tutorial){
         this.tutorialId = tutorial.getTutorialId();
         this.title = tutorial.getTitle();
         this.content = tutorial.getContent();
 		this.instructor = tutorial.getInstructor();
+		this.project = tutorial.getProject();
     }
 }
