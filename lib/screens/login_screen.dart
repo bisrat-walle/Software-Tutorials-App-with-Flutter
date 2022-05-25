ffbd6c6d41269e934409d0f10e61f2713fbec3ef
Bamlaku <bamlakhiruya@gmail.com>
2022-05-25 13:47:01 +0300

    creating login_page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,114 +1,173 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:softwaretutorials/models/login_response.dart';
+import 'package:softwaretutorials/screens/screens.dart';
 import '../api/api_service.dart';
-import '../models/login_response.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:softwaretutorials/models/models.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
-
+  
+  static MaterialPage page() {
+	return MaterialPage(
+		name: TutorialPages.loginPath,
+		key: ValueKey(TutorialPages.loginPath),
+		child: LoginScreen(),
+		);
+	}
+  
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _loginFormKey = GlobalKey<FormState>();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _usernameController;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _usernameController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
-      body: Container(
-        color: const Color.fromARGB(0, 34, 34, 204),
-        padding: const EdgeInsets.all(30),
-        child: Form(
-          key: _loginFormKey,
-          child: Container(
-            constraints: const BoxConstraints(
-              maxHeight: 150,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextFormField(
-                  controller: _usernameController,
-                  style: const TextStyle(
-                    fontSize: 17,
-                  ),
-                  decoration: const InputDecoration(
-                    labelText: "Enter your username",
-                    contentPadding: EdgeInsets.fromLTRB(1, 0, 0, 5),
-                    prefixIcon: Padding(
-                      padding: EdgeInsets.only(top: 5),
-                      child: Icon(Icons.account_circle),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            children: [
+              SvgPicture.asset(
+                "assets/images/background.svg",
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+              Positioned(
+                top: 100,
+                left: MediaQuery.of(context).size.width / 2 - 64,
+                child: SvgPicture.asset("assets/images/brain.svg"),
+              ),
+              Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    height: 300,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10))),
+                    child: SingleChildScrollView(
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 35, right: 35),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 25),
+                                  Container(
+                                    constraints: BoxConstraints(maxWidth: 450),
+                                    child: TextFormField(
+                                        controller: _usernameController,
+                                        validator: (value) {
+                                          if (value == null ||
+                                              value.isEmpty ||
+                                              value.length < 5) {
+                                            return "Username is required and must be at least 5 characters";
+                                          }
+                                          return null;
+                                        },
+                                        decoration: InputDecoration(
+                                            fillColor: Colors.grey.shade100,
+                                            filled: true,
+                                            hintText: "Username",
+                                            border: OutlineInputBorder(
+                                                borderRadius:
+                                                BorderRadius.circular(10)))),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                              Container(
+                                constraints: BoxConstraints(maxWidth: 450),
+                                child:TextFormField(
+                                    controller: _passwordController,
+                                    validator: (value) {
+
+                                      if (value == null ||
+                                          value.isEmpty ||
+                                          value.length < 5) {
+                                        return "Password is required and must be at least 8 characters";
+                                      }
+                                      return null;
+                                    },
+                                    decoration: InputDecoration(
+                                        fillColor: Colors.grey.shade100,
+                                        filled: true,
+                                        hintText: "Password",
+                                        border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10))),
+                                    style: TextStyle(),
+                                    obscureText: true,
+                                  ),),
+                                  SizedBox(height: 15),
+                                  SizedBox(
+                                      height: 50,
+                                      width: 450,
+                                      child: ElevatedButton(
+
+                                        style: ButtonStyle(
+                                          backgroundColor: MaterialStateProperty
+                                              .resolveWith<Color>(
+                                            (Set<MaterialState> states) {
+                                              if (states.contains(
+                                                  MaterialState.pressed))
+                                                return Colors.blue;
+                                              return Colors
+                                                  .teal; // Use the component's default.
+                                            },
+                                          ),
+                                        ),
+                                        child: Text("Login"),
+                                        onPressed: () async {
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                            Provider.of<AppStateManager>(context, listen:false).login(
+                                              username: this._usernameController.text,
+                                              password: this._passwordController.text
+                                            );
+                                          }
+                                        },
+                                      )),
+                                  
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                    prefixStyle: TextStyle(height: 100.0),
-                  ),
-                  autofocus: true,
-                  validator: (value) {
-                    if (value.toString().length < 5 ||
-                        value == null ||
-                        value.isEmpty) {
-                      return 'password is required and must be at least 6 characters';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  style: const TextStyle(
-                    fontSize: 17,
-                  ),
-                  decoration: const InputDecoration(
-                    labelText: "Enter your password",
-                    contentPadding: EdgeInsets.fromLTRB(1, 0, 0, 5),
-                    prefixIcon: Padding(
-                      padding: EdgeInsets.only(top: 5),
-                      child: Icon(Icons.password_rounded),
-                    ),
-                    prefixStyle: TextStyle(height: 100.0),
-                  ),
-                  autofocus: true,
-                  validator: (value) {
-                    if (value.toString().length < 5 ||
-                        value == null ||
-                        value.isEmpty) {
-                      return 'username is required and must be at least 5 characters';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    print({
-                      "username": _usernameController.text,
-                      "password": _passwordController.text
-                    });
-                    final res = await authenticateUser(username:_usernameController.text, password: _passwordController.text);
-                    print(res.accessToken);
-                    if (_loginFormKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                  },
-                  child: const Text('Submit'),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+                  ))
+            ],
+          )),
     );
   }
 
   @override
   void dispose() {
-    super.dispose();
     _usernameController.dispose();
     _passwordController.dispose();
+    super.dispose();
   }
 }
