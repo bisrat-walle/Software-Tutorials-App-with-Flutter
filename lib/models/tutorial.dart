bb59e7d187985f594ccf5919384b231b862c4aa2
Abraham <abrahamshimekt4096@gmail.com>
2022-05-25 18:55:46 +0300

    tutorial model created to to allow users register for tutorials

---
+++ lib/models/tutorial.dart
@@ -0,0 +1,35 @@
+import 'models.dart';
+
+class Tutorial{
+    int? tutorialId;
+    String? title;
+    String? content;
+	bool? enrolled;
+    String? submittedLink;
+	User? instructor;
+	int? enrollementCount;
+	Project? project;
+    Tutorial({this.enrollementCount, this.tutorialId, this.content, this.title, this.enrolled,
+				this.submittedLink, this.instructor, this.project});
+
+    @override
+		String toString(){
+    	return "Tutorial {title: $title}";
+		}
+	factory Tutorial.fromJson(Map<String, dynamic?>? json) {
+		if (json == null){
+			return Tutorial();
+		}
+		return Tutorial(
+		  tutorialId: json['tutorialId'],
+		  title: json['title'],
+		  enrolled: json['enrolled'],
+		  content: json['content'],
+		  submittedLink: json['submittedLink'],
+		  instructor: User.fromJson(json['instructor']),
+		  project: Project.fromJson(json['project']),
+			enrollementCount: json['enrollementCount']
+		);
+    }
+	
+}
