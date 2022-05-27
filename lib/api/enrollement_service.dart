c0e3af317f1dec917a2bf38be31787b41a0d006b
ysihak-B <ysihakbazezew@gmail.com>
2022-05-27 13:03:51 +0300

    api for enrollement

---
+++ lib/api/enrollement_service.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/physics.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/http.dart';
+import 'package:softwaretutorials/api/token_interceptor.dart';
+import 'dart:convert';
+import '../models/tutorial.dart';
+
+String _baseUrl = "http://localhost:8080/api/v1/tutorials/";
+
+http.Client client = InterceptedClient.build(interceptors: [
+	TokenInterceptor(),
+]);
+
+class EnrollementService{
+
+	static Future<String?> enroll({required int tutorialId}) async {
+		try{
+			final response = await client.post(
+					Uri.parse(_baseUrl+tutorialId.toString()+"/enroll")
+			);
+
+			if (response.statusCode == 204) {
+				return "Course successfully enrolled";
+			}
+		} catch(e){
+			print(e);
+			return "Unable to enroll";
+		}
+	}
+
+}
