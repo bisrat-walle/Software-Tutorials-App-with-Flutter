100d64bc8535ccfbe7d19a53082dc041d42d9955
bisrat-walle <bisratwalle3@gmail.com>
2022-06-02 12:01:18 +0300

    removing folders which violate ddd architecture

--- front-end/lib/models/models.dart
+++
@@ -1,6 +0,0 @@
-export 'app_state_manager.dart';
-export 'project.dart';
-export 'tutorial.dart';
-export 'user.dart';
-export 'login_response.dart';
-export 'tutorial_pages.dart';
--- front-end/lib/screens/screens.dart
+++
@@ -1,6 +0,0 @@
-export 'login_screen.dart';
-export 'tutorial_screen.dart';
-export 'create_tutorial_screen.dart';
-export 'tutorial_detail_screen.dart';
-export 'signup_screen.dart';
-export 'splash_screen.dart';
