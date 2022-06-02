87750e7d63d776cbfa719180bcd2e69780163635
Bamlaku <bamlakhiruya@gmail.com>
2022-06-02 18:03:40 +0300

    adding a method that adds users

--- front-end/lib/infrastructure/local_repository/user_local_repository.dart
+++ front-end/lib/infrastructure/local_repository/user_local_repository.dart
@@ -6,4 +6,11 @@ import 'local_database.dart';
 class UserLocalRepository {
   final dbProvider = TutorialDataProvider.dbProvider;
 
+  //Adds new User records
+  Future<int> createUser(User user) async {
+    final db = await dbProvider.database;
+    var result = db.insert(userTable, user.toJson());
+    return result;
+  }
+
 }
