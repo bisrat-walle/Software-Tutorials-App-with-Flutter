fe3fd3a8af72053051c9e2ee2a77bd2bcadd36ea
Bamlaku <bamlakhiruya@gmail.com>
2022-06-02 18:02:34 +0300

    creating a user repo that do crud operations on users

---
+++ front-end/lib/infrastructure/local_repository/user_local_repository.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'package:softwaretutorials/domain/models/models.dart';
+
+import 'local_database.dart';
+
+class UserLocalRepository {
+  final dbProvider = TutorialDataProvider.dbProvider;
+
+}
