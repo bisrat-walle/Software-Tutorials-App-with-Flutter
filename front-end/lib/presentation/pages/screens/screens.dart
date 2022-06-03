77a955be59de6757bf8fab99cdf8d45ec426f034
ysihak-B <ysihakbazezew@gmail.com>
2022-06-03 10:00:43 +0300

    adding user signup and splash screens

---
+++ front-end/lib/presentation/pages/screens/screens.dart
@@ -0,0 +1,6 @@
+export '../signin/login_screen.dart';
+export 'tutorial/tutorial_screen.dart';
+export 'tutorial/create_tutorial_screen.dart';
+export 'tutorial/tutorial_detail_screen.dart';
+export 'signup_screen.dart';
+export 'splash_screen.dart';
