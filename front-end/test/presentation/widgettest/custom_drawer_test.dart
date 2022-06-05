279049b6db72a90c1bf53aaf1a9e4c3317ebe95e
Abraham <abrahamshimekt4096@gmail.com>
2022-06-05 23:36:25 +0300

    closes #8

---
+++ front-end/test/presentation/widgettest/custom_drawer_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:softwaretutorials/presentation/pages/components/components.dart';
+
+
+
+void main() {
+  testWidgets('Drawer should not contain logout', (WidgetTester tester) async {
+    final GlobalKey<ScaffoldState> _key = GlobalKey(); 
+    await tester.pumpWidget( MaterialApp(
+      home: Scaffold(
+        key: _key,
+        appBar: AppBar(title: Text("Home",),      
+      ),
+      
+      drawer: Builder(
+        builder: (context) {
+          return CustomDrawer.get(context);
+        }
+      ),
+      )
+
+
+    )
+
+    
+
+      
+    );
+  await tester.pumpAndSettle();
+    _key.currentState!.openDrawer();
+    expect(find.text("hello"), findsNothing);
+  });
+}
